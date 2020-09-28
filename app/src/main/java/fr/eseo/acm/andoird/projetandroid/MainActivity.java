8280c65314d320ba59c5f8a4768ffd347fd70bba
Mathe <annecamille.mathe@reseau.eseo.fr>
2020-09-28 14:35:53 +0200

    creation projet anne c

--- app/src/main/java/fr/eseo/acm/andoird/projetandroid/MainActivity.java
+++ app/src/main/java/fr/eseo/acm/andoird/projetandroid/MainActivity.java
@@ -2,13 +2,55 @@ package fr.eseo.acm.andoird.projetandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        //R.menu.menu est l'id de notre menu
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case R.id.item1:
+                openActivityProject();
+                break;
+            case R.id.item2:
+                openActivityJury();
+                break;
+                case R.id.item3:
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void openActivityProject(){
+        Intent intent = new Intent(this, ProjetActivity.class);
+        startActivity(intent);
+    }
+
+    public void openActivityJury(){
+        Intent intent = new Intent(this, JuryActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
