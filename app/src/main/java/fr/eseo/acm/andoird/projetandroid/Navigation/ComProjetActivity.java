f4be37646ee1c52031cc297762652f17f3aec546
Anne Camille <annecamille.mathe@reseau.eseo.fr>
2020-09-30 16:15:34 +0200

    retablissement du menu projets

--- app/src/main/java/fr/eseo/acm/andoird/projetandroid/Navigation/ComProjetActivity.java
+++ app/src/main/java/fr/eseo/acm/andoird/projetandroid/Navigation/ComProjetActivity.java
@@ -1,15 +1,43 @@
 package fr.eseo.acm.andoird.projetandroid.Navigation;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import fr.eseo.acm.andoird.projetandroid.Fragments.ProjectsSummaryFragment;
 import fr.eseo.acm.andoird.projetandroid.R;
 
 public class ComProjetActivity extends AppCompatActivity {
+    private ProjectsSummaryFragment projectsSummaryFragment;
+    /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        String defaultValue = getResources().getString(R.string.saved_token);
+        String token = sharedPref.getString(getString(R.string.saved_token), defaultValue);
+        System.out.print("REUSSI ? " + token);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project);
+    }*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d("message = ", "lancement des projets");
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        String defaultValue = getResources().getString(R.string.saved_token);
+        String token = sharedPref.getString(getString(R.string.saved_token), defaultValue);
+        Log.d("TOKEN = ", token);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_com);
+        this.projectsSummaryFragment = ProjectsSummaryFragment.newInstance();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.placeholder_projects_summary_list, projectsSummaryFragment);
+        transaction.commit();
+
     }
+
 }
