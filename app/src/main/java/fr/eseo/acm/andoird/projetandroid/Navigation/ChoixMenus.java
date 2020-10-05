4250d27ec1fbaeed2b5946ed55238ec49290a955
Anne Camille <annecamille.mathe@reseau.eseo.fr>
2020-10-05 13:58:33 +0200

    Affichage des projets

--- app/src/main/java/fr/eseo/acm/andoird/projetandroid/Navigation/ChoixMenus.java
+++ app/src/main/java/fr/eseo/acm/andoird/projetandroid/Navigation/ChoixMenus.java
@@ -1,13 +1,24 @@
 package fr.eseo.acm.andoird.projetandroid.Navigation;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import fr.eseo.acm.andoird.projetandroid.R;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class ChoixMenus extends AppCompatActivity {
+import fr.eseo.acm.andoird.projetandroid.API.API;
+import fr.eseo.acm.andoird.projetandroid.Fragments.ListProjectsFragment;
+import fr.eseo.acm.andoird.projetandroid.R;
+
+import java.io.IOException;
+import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+public class ChoixMenus extends API {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
@@ -22,7 +33,19 @@ public class ChoixMenus extends AppCompatActivity {
 
         switch (item.getItemId()) {
             case R.id.item1:
-                openActivityProject();
+                try {
+                    openActivityProject();
+                } catch (CertificateException e) {
+                    e.printStackTrace();
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                } catch (KeyStoreException e) {
+                    e.printStackTrace();
+                } catch (KeyManagementException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
             case R.id.item2:
                 openActivityJury();
@@ -35,8 +58,11 @@ public class ChoixMenus extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void openActivityProject() {
-        Intent intent = new Intent(this, AllProjectsActivity.class);
+    public void openActivityProject() throws CertificateException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException {
+        //TEST
+        //Intent intent = new Intent(this, AllProjectsActivity.class);
+        Intent intent = new Intent(this, ListProjectsFragment.class);
+        intent.putExtra("json", this.getProjects());
         startActivity(intent);
     }
 
@@ -48,6 +74,16 @@ public class ChoixMenus extends AppCompatActivity {
     public void openActivityNotesCom() {
         Intent intent = new Intent(this, ComNotesActivity.class);
         startActivity(intent);
+    }
+
+    public String getProjects() throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException, KeyManagementException {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String username = sharedPref.getString("saved_username", "le login n'est pas trouvé");
+        String token = sharedPref.getString("saved_token", "le token n'est pas trouvé");
+        String[] params = new String[] {API.API_USER, username, API.API_TOKEN, token};
+        URL url = this.buildRequest(API.API_PROJECTS, params);
+        System.out.println(url.toString());
+       return this.getReplyFromHttpUrl(url);
     }
 
 }
--- app/src/main/java/fr/eseo/acm/andoird/projetandroid/room/Project.java
+++ app/src/main/java/fr/eseo/acm/andoird/projetandroid/room/Project.java
@@ -1,4 +1,78 @@
 package fr.eseo.acm.andoird.projetandroid.room;
 
 public class Project {
+
+    private int idProject;
+    private String title;
+    private String description;
+    private String superviseur;
+    private boolean poster;
+    private int confidentiality;
+
+    public Project(int idProject, String titre, String description, String superviseur, boolean poster, int confidentiality) {
+        this.idProject = idProject;
+        this.title = titre;
+        this.description = description;
+        this.superviseur = superviseur;
+        this.poster = poster;
+        this.confidentiality = confidentiality;
+    }
+
+    public Project(String titre, String superviseur) {
+        this.title = titre;
+        this.superviseur = superviseur;
+    }
+
+    public int getIdProject() {
+        return idProject;
+    }
+
+    public void setIdProject(int idProject) {
+        this.idProject = idProject;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String titre) {
+        this.title = titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSuperviseur() {
+        return superviseur;
+    }
+
+    public void setSuperviseur(String superviseur) {
+        this.superviseur = superviseur;
+    }
+
+    public boolean getPoster() {
+        return poster;
+    }
+
+    public void setPoster(boolean poster) {
+        this.poster = poster;
+    }
+
+    public int getConfidentiality() {
+        return confidentiality;
+    }
+
+    public void setConfidentiality(int confidentiality) {
+        this.confidentiality = confidentiality;
+    }
+
+    @Override
+    public String toString()  {
+        return this.title +" (Description: "+ this.description+")";
+    }
 }
