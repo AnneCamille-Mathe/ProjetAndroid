2c0b6f2a6dbecee36816c2e3f2ab0c412be5d005
Octave Victor <victor.octave@reseau.eseo.fr>
2020-10-13 19:22:46 +0200

    Affichage popup si identifiants invalides

--- app/src/main/java/fr/eseo/acm/andoird/projetandroid/Navigation/Connexion.java
+++ app/src/main/java/fr/eseo/acm/andoird/projetandroid/Navigation/Connexion.java
@@ -1,13 +1,17 @@
 
 package fr.eseo.acm.andoird.projetandroid.Navigation;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import java.io.IOException;
 import java.net.URL;
 import java.security.KeyManagementException;
@@ -58,13 +62,14 @@ public class Connexion extends API {
         editor.putString("saved_username", username.getText().toString());
         editor.commit();
 
-        int role = this.getRole(username.getText().toString());
-
-        //Ajouter ici le menu
         if(token != null){
+            int role = this.getRole(username.getText().toString());
             Intent intent = new Intent(this, ChoixMenus.class);
             intent.putExtra("role", role);
             startActivity(intent);
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Vos identifiants sont invalides, veuillez réessayer !", Toast.LENGTH_LONG).show();
         }
     }
 
--- app/src/main/java/fr/eseo/acm/andoird/projetandroid/servicesAPI/LOGONService.java
+++ app/src/main/java/fr/eseo/acm/andoird/projetandroid/servicesAPI/LOGONService.java
@@ -2,6 +2,9 @@ package fr.eseo.acm.andoird.projetandroid.servicesAPI;
 
 public class LOGONService {
     public String getToken(String url){
+        if(url.contains("KO")){
+            return null;
+        }
         String token = "";
         String[] details = url.split("\"token\": \"");
         String[] detail = details[1].split("\"");
