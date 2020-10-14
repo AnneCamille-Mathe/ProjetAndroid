168e477cea887cbb93e49384cf7487f786d82947
Anne Camille <annecamille.mathe@reseau.eseo.fr>
2020-10-14 14:25:19 +0200

    Ajout des commentaires

--- app/src/main/java/fr/eseo/acm/andoird/projetandroid/Fragments/NotesComActivity.java
+++ app/src/main/java/fr/eseo/acm/andoird/projetandroid/Fragments/NotesComActivity.java
@@ -29,15 +29,20 @@ public class NotesComActivity extends AppCompatActivity {
         setContentView(R.layout.fragment_notes_com);
         String position = "";
 
-        if(getIntent().hasExtra("position")) {
+        if (getIntent().hasExtra("position")) {
             position = (getIntent().getStringExtra("position"));
         }
 
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         String notes = sharedPref.getString("notes", "");
+        String commentaires = sharedPref.getString("commentaires", "");
 
-        if(notes.charAt(0)==('-')){
+        if (notes.charAt(0) == ('-')) {
             notes = notes.substring(1);
+        }
+
+        if (commentaires.charAt(0) == ('-')) {
+            commentaires = commentaires.substring(1);
         }
 
         //Récuperer l'ID du projet a partir de la position
@@ -55,28 +60,44 @@ public class NotesComActivity extends AppCompatActivity {
         List<String> notesAAfficher = new ArrayList<String>();
         String[] listeNotes = notes.split("-");
 
-        for(int i = 0; i< listeNotes.length; i++){
+        for (int i = 0; i < listeNotes.length; i++) {
             String[] notesEtId = listeNotes[i].split("/");
             if (Integer.parseInt(notesEtId[1]) == idProject) {
                 notesAAfficher.add(notesEtId[0]);
             }
         }
 
+        List<String> commentairesAAfficher = new ArrayList<String>();
+        String[] listeCommentaires = commentaires.split("-");
+
+        for (int i = 0; i < listeCommentaires.length; i++) {
+            String[] commentaireEtId = listeCommentaires[i].split("/");
+            if (Integer.parseInt(commentaireEtId[1]) == idProject) {
+                commentairesAAfficher.add(commentaireEtId[0]);
+            }
+        }
+
         //afficher les notes
         String afficheNote = "";
-        for(int i = 0; i< notesAAfficher.size(); i++){
+        for (int i = 0; i < notesAAfficher.size(); i++) {
             afficheNote = afficheNote + " / " + notesAAfficher.get(i);
         }
         TextView textNotes = findViewById(R.id.textNotes);
 
         //TODO - If not null supprimer les 2 premiers caractere (substring(1).substring(1))
-
         textNotes.setText(afficheNote);
 
-        /*
+
+        String afficheCom = "";
+        for (int i = 0; i < commentairesAAfficher.size(); i++) {
+            afficheCom = afficheCom + " / " + commentairesAAfficher.get(i);
+        }
+
         TextView textCommentaire = findViewById(R.id.textCommentaire);
-        textCommentaire.setText(titre);
-        */
+
+        //TODO - If not null supprimer les 2 premiers caractere (substring(1).substring(1))
+
+        textCommentaire.setText(afficheCom);
 
     }
 }
