f5732962d46075af16f310d51c0c92b1ea51a473
Octave Victor <victor.octave@reseau.eseo.fr>
2020-10-19 14:25:24 +0200

    Ajout notation individuelle

--- app/src/main/java/fr/eseo/acm/andoird/projetandroid/Fragments/JuryNotesActivity.java
+++ app/src/main/java/fr/eseo/acm/andoird/projetandroid/Fragments/JuryNotesActivity.java
@@ -4,14 +4,17 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.json.JSONException;
-
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 import fr.eseo.acm.andoird.projetandroid.API.API;
 import fr.eseo.acm.andoird.projetandroid.R;
@@ -20,17 +23,43 @@ public class JuryNotesActivity extends API {
 
     private String position;
 
+    private String[] project;
+
+    private HashMap<Integer, String> students_list = new HashMap<>();
+
     private float note;
+
+    private String student;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
 
-        Button button = findViewById(R.id.validerNote);
-        TextView givenNote = findViewById(R.id.givenNote);
+        if(getIntent().hasExtra("idProject")) {
+            position = getIntent().getStringExtra("position");
+        }
 
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String projects = sharedPref.getString("projectsFromJury", "Projets non trouvés !");
+        String[] splitted_projects = projects.split("\\}]\\}, ");
+        project = splitted_projects[Integer.parseInt(position)].split("userId");
+        students_list.put(-1, "Tous");
+        for(int i=1; i<project.length; i++) {
+            String[] data = project[i].split("\"");
+            int idStudent = Integer.parseInt(project[i].split(",\"")[0].substring(2));
+            students_list.put(idStudent, data[8]+" "+data[4]);
+        }
+
+        Button button = findViewById(R.id.validerNote);
+        TextView givenNote = findViewById(R.id.givenNote);
+        final Spinner students = findViewById(R.id.spinnerStudents);
+        students.setVisibility(View.VISIBLE);
+
+        String[] items = students_list.values().toArray(new String[0]);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_dropdown_students_list, items);
+        students.setAdapter(adapter);
+
         final String noteProjet = sharedPref.getString("noteProjet"+getIntent().getIntExtra("idProject", 0),
                                                 "Pas de note attribuée");
 
@@ -42,41 +71,42 @@ public class JuryNotesActivity extends API {
             public void onClick(View v) {
                 EditText noteEdit = findViewById(R.id.noteVisitor);
                 note = Float.parseFloat(noteEdit.getText().toString());
-                if(note != Float.parseFloat(noteProjet)) {
-                    JuryNotesActivity.this.enregistrerNote(v);
-                }
-                else{
+                student = students.getSelectedItem().toString();
+
+                if(note == Float.parseFloat(noteProjet) && student.equals("Tous")) {
                     Toast.makeText(getApplicationContext(), "Vous devez saisir une note différente de celle déjà attribuée !", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    JuryNotesActivity.this.enregistrerNote(v);
                 }
             }
         });
-
-        if(getIntent().hasExtra("idProject")) {
-            position = getIntent().getStringExtra("position");
-        }
     }
 
     public void enregistrerNote(View v) {
         boolean valid = true;
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        String projects = sharedPref.getString("projectsFromJury", "Projets non trouvés !");
-        String[] splitted_projects = projects.split("\\}]\\}, ");
-        String[] splitted_project = splitted_projects[Integer.parseInt(position)].split("userId");
-        for(int i=1; i<splitted_project.length; i++){
-            int idStudent = Integer.parseInt(splitted_project[i].split(",\"")[0].substring(2));
-            String answer = this.noter(idStudent);
-            if(answer.contains("KO")){
-                valid = false;
+
+        for(int i=1; i<project.length; i++){
+            int idStudent = Integer.parseInt(project[i].split(",\"")[0].substring(2));
+            if(student.equals("Tous") || student.equals(students_list.get(idStudent))){
+                String answer = this.noter(idStudent);
+                if(answer.contains("KO")){
+                    valid = false;
+                }
             }
         }
-        if(valid){
+        if(!valid){
+            Toast.makeText(getApplicationContext(), "Un problème est survenu lors de l'envoi d'une note au moins !", Toast.LENGTH_LONG).show();
+        }
+        else if (student.equals("Tous")){
+            SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
             SharedPreferences.Editor editor = sharedPref.edit();
             editor.putString("noteProjet"+getIntent().getIntExtra("idProject", 0), String.valueOf(note));
             editor.commit();
             this.finish();
         }
-        else {
-            Toast.makeText(getApplicationContext(), "Un problème est survenu lors de l'envoi des notes !", Toast.LENGTH_LONG).show();
+        else{
+            this.finish();
         }
     }
 
